b8c2f00378cac8989674e761565bc975b7e69197
DasunNirmal <dasun200301@gmail.com>
2024-06-29 10:05:05 +0530

    Updated

--- src/main/java/lk/ijse/jakartaeeintro/StudentWildCardMapping.java
+++ src/main/java/lk/ijse/jakartaeeintro/StudentWildCardMapping.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet(value = "/student/*")
+@WebServlet(value = "/*")
 public class StudentWildCardMapping extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
